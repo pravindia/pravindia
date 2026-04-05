da42de69b043acec76af2d97afdb868db5a45844
pravindia <pravinda4hero@gmail.com>
2026-04-05 22:41:01 +0530

    remove contact

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
                     <li><Link href="/#works">About</Link></li>
                     <li><Link href="/blog">Blog</Link></li>
                     <li>
-                        <a href="https://wa.me/+918870142519?text=" target="_blank" rel="noreferrer" >
+                        <a href="https://www.linkedin.com/in/pravindia" target="_blank" rel="noreferrer" >
 
                             <span className={styles.openToWork}>Open to work</span>
                         </a>
